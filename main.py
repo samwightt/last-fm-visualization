5b76a173ab1299001eea2abf24ef485a4d284b34
Sam Wight <samuelwight@gmail.com>
2019-01-11 16:12:45 -0600

    Removed function that did nothing.

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ from apiKey import apiKey
 
 redisClient = redis.Redis()
 
-def add_tracks_to_database(tracks):
-    print("Doing!")
-    print(tracks)
-
 def add_user_to_queue(username):
     url = 'https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + username + '&api_key=' + apiKey + '&format=json&limit=200'
     r = requests.get(url)
