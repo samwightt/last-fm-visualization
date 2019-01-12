389845e505ef8138327b2da1d2310df7ba5d2c70
Sam Wight <samuelwight@gmail.com>
2019-01-12 00:16:16 -0600

    Created skeleton cron python process, added new functions in main.

---
+++ cron.py
@@ -0,0 +1,23 @@
+import requests
+import redis
+import time
+
+from apiKey import api_key
+from main import get_user_page, process_user_page
+
+redis_client = redis.Redis()
+
+while True:
+    while redis_client.llen('userQueue') > 0:
+        current_user = redis_client.lpop('userQueue').decode('utf-8')
+        current_page = get_user_page(current_user, 1)
+
+        process_user_page(current_page)
+
+        total_pages = int(current_page['recenttracks']['@attr']['totalPages'])
+
+        for i in range(2, total_pages):
+            current_page = get_user_page(current_user, i + 1)
+            process_user_page(current_page)
+
+    time.sleep(1)
--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import requests
 import redis
-from apiKey import apiKey
+from apiKey import api_key
 
-redisClient = redis.Redis()
+redis_client = redis.Redis()
 
+# Adds user to the processing queue in Redis.
+# Raises a LookupError if the user was not found on LastFM, and raises RuntimeErrors if the user is already in the
+# queue or has already been processed.
 def add_user_to_queue(username):
-    url = 'https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + username + '&api_key=' + apiKey + '&format=json&limit=200'
-    r = requests.get(url)
-    content = r.json()
+    content = get_user_page(username, 1)
 
-    if not 'error' in content:
-        redisClient.lpush('userQueue', username)
+    if 'error' in content:
+        raise LookupError('UsernameNotFound', 'No user with that username was found on LastFM.')
+
+    elif redis_client.sismember('finishedUsers', username):
+        raise RuntimeError('UsernameFinished', 'That user has already been processed.')
+
+    elif redis_client.sismember('inUserQueue', username):
+        raise RuntimeError('UsernameInQueue', 'That user is already in the queue.')
+
+    else:
+        redis_client.lpush('userQueue', username)
+        redis_client.sadd('inUserQueue', username)
+
+# Gets a page of the user's history given a username and a page.
+def get_user_page(username, page):
+    string_page = str(page)
+    url = 'https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&page=' + string_page + '&user=' + username + '&api_key=' + api_key + '&format=json&limit=200'
+    return requests.get(url).json()
+
+# Given a plain JSON object of the page, process and store the page's content in the database.
+def process_user_page(page_content):
+   tracks = page_content['recenttracks']['track']
+
+   for track in tracks:
+       print(track['name'])
